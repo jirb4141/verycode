8ffcf08681c82eeb326b45b270a0d9cbb502e007
jirb4141 <55462989+jirb4141@users.noreply.github.com>
2019-11-07 13:01:09 -0600

    Update VeryProject.java

--- VeryProject/src/veryproject/VeryProject.java
+++ VeryProject/src/veryproject/VeryProject.java
@@ -17,7 +17,7 @@ public class VeryProject {
     public static void main(String[] args) {
         // TODO code application logic here
         int numerodiez = 0;
-        
+        // testing pull from upstream        
     }
     
 }
