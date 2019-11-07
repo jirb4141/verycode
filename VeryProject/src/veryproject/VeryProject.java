ad96d11f8164d92f32c8337d8434f73031344afe
jirb4 <jirb4@10.233.131.121>
2019-11-07 12:57:41 -0600

    prueba hola

---
+++ VeryProject/src/veryproject/VeryProject.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package veryproject;
+
+/**
+ *
+ * @author jirb4
+ */
+public class VeryProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int numerodiez = 0;
+        
+    }
+    
+}
